2a167cf891963c0cf69def25411a274b202b96b8
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-11-16 12:22:34 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -16,6 +16,7 @@ import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
 import lime.app.Application;
+import mobile.states.CopyState;
 
 #if android
 import android.content.Context;
@@ -44,7 +45,7 @@ class Main extends Sprite
 	var game = {
 		width: 1280, // WINDOW width
 		height: 720, // WINDOW height
-		initialState: TitleState, // initial game state
+		initialState: CopyState, // initial game state
 		zoom: -1.0, // game state bounds
 		framerate: 60, // default framerate
 		skipSplash: true, // if the default flixel splash screen should be skipped
